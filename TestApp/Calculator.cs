99f8f663c864d5ad59d216d4946d20b05cb77bfd
shirishagam <98177681+shirishagam@users.noreply.github.com>
2024-05-05 23:48:28 +0530

    Calculator and unit tests (#1)
    * introduced calculator
    
    * intro calcu
    
    * gitignore file
    
    * introducing test project

---
+++ TestApp/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp;
+    
+    public class Calculator
+    {
+    public int Add(int x, int y) => x + y;
+    public int Subtract(int x, int y) => x - y;
+    public int Multiply(int x, int y) => x * y;
+    public int Divide(int x, int y) => x / y;
+
+    }
+

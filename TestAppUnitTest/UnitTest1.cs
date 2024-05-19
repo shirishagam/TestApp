db1b3d46df990bc91e4ccc58fd9778db133d8dbb
shirishagam <98177681+shirishagam@users.noreply.github.com>
2024-05-19 23:56:44 +0530

    Calculator and unit tests (#2)
    * introduced calculator
    
    * intro calcu
    
    * gitignore file
    
    * introducing test project
    
    * added test cases and coverage
    
    * Divide two integers coverage
    
    * all sync

--- TestAppUnitTest/UnitTest1.cs
+++ TestAppUnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestApp;
 
 namespace TestAppUnitTest;
@@ -11,5 +12,24 @@ public class CalculatorUnitTest
     {
         Calculator c = new Calculator();
         Assert.AreEqual(2, c.Add(1, 1));
+    }
+
+    [TestMethod("Subtract two integers")]
+    public void SubtractIntegers()
+    {
+        Calculator c = new Calculator();
+        Assert.AreEqual(0, c.Subtract(1, 1));
+    }
+    [TestMethod("Multiply two integers")]
+    public void MultiplyIntegers()
+    {
+        Calculator c = new Calculator();
+        Assert.AreEqual(1, c.Multiply(1, 1));
+    }
+    [TestMethod("Divide two integers")]
+    public void DivideIntegers()
+    {
+        Calculator c = new Calculator();
+        Assert.AreEqual(2, c.Divide(1, 1));
     }
 }
